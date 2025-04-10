78cc61a73b9bedfede716519716e979945aa325c
Hungtx2tpb <hungtx2409999@gmail.com>
2025-04-10 12:57:35 +0700

    update

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -2,8 +2,8 @@ import { loginTest } from "../../src/setup/page-setup";
 
 loginTest.describe("TC_1 test login @smoke", () => {
     loginTest('test login with valid credentials @smoke', async ({ homePage, loginPage }) => {
-      let url = process.env.URL || '';
-      let market = process.env.market || '';
+      // let url = process.env.URL || '';
+      // let market = process.env.market || '';
       await homePage.goToHomePage();
       // await homePage.verifyGoToHomePageSuccessfully(url, market);
       // await loginPage.loginToHomePage('fatibe7647@operades.com', '927445');
